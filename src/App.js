4fb7fa5a110218eff8601712de708426d930445c
VladShev74 <vladadshev@gmail.com>
2025-01-23 03:35:04 +0100

    Added "back to main page" button for ServicePage.js + some minor fixes

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import axios from "axios";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import ServicePage from './ServicePage'; // Імпортуємо компонент ServicePage
 
-import './App.css'; 
+import './App.css';
 
 <script src="https://telegram.org/js/telegram-web-app.js"></script>
 
@@ -15,7 +15,7 @@ function App() {
     try {
       // const response = await axios.get("https://glazoff-bot-experimental.onrender.com/services");
       const response = await axios.get("http://127.0.0.1:5000/services");
-      
+
       setServices(response.data); // Зберігаємо дані в стані
     } catch (error) {
       console.error("Помилка при отриманні даних:", error);
@@ -36,21 +36,21 @@ function App() {
             </a>
           </div>
           <div className="support-button-container">
-            <a 
-              href="https://t.me/Glazoff_com" 
-              target="_blank" 
-              rel="noopener noreferrer" 
+            <a
+              href="https://t.me/Glazoff_com"
+              target="_blank"
+              rel="noopener noreferrer"
               className="support-button"
             >
               Зв'язатися з підтримкою
             </a>
           </div>
         </header>
-        
+
         <main className="container">
           <Routes>
-            <Route 
-              path="/" 
+            <Route
+              path="/"
               element={
                 <>
                   <h1 className="title">Список послуг</h1>
@@ -66,9 +66,9 @@ function App() {
                         </div>
                         <div className="service-details">
                           <h2 className="service-name">{service.service_name}</h2>
-                          <p className="service-price">{service.service_p}</p>
-                          <p className="service-description"><strong>Опис:</strong> {service.service_price}</p>
-                          <Link to={`/service/${service.service_id}`} className="order-button">
+                          <p className="service-price">{service.service_price}</p>
+                          <p className="service-description"><strong>Опис:</strong> {service.service_p}</p>
+                          <Link to={`/service/${service.service_id}`} onClick={() => window.scrollTo(0, 0)} className="order-button">
                             Замовити послугу
                           </Link>
                         </div>
@@ -78,9 +78,9 @@ function App() {
                 </>
               }
             />
-            <Route 
-              path="/service/:serviceId" 
-              element={<ServicePage services={services} />} 
+            <Route
+              path="/service/:serviceId"
+              element={<ServicePage services={services} />}
             />
           </Routes>
         </main>
